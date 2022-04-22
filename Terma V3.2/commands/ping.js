219a6616c08048bb8f483d661a408681c79325a3
spydude2w1 <72872888+spydude2w1@users.noreply.github.com>
2022-04-22 23:16:52 +0530

    Delete Terma V3.2 directory

--- Terma V3.2/commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: "ping",
-  permissions: ["SEND_MESSAGES"],
-  execute(client, message, args){
-    message.channel.send(`Host ping - **${Date.now() - message.createdTimestamp}** ms`)
-
-  }
-}
